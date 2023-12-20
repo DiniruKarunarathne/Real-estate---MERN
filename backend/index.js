cb9d1ace3950756eaa0f2c710d3637381c2c338e
Diniru <Dinirukaru65@gmail.com>
2023-12-20 14:01:47 +0530

    connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MongoDB).then(() =>{console.log("MongoDB connected")}).catch((err) =>{console.log(err)});
 const app = express();
 
 const PORT = process.env.PORT || 8070 ; 
