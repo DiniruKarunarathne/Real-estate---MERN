fcf5493ca9616e37949568021918d2f485d3563a
Diniru <Dinirukaru65@gmail.com>
2023-12-20 20:07:46 +0530

    Create signup route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'; //import bcryptjs (encrypt password)
+
+export const signup = async (req, res) => {
+    const { username , email, password } = req.body; //get data from frontend
+    const hashedPassword = bcryptjs.hashSync(password, 10); //encrypt password
+    const newUser = new User({ username,email,password:hashedPassword}); //create new user
+    try{
+        await newUser.save();
+        res.status(201).json('User create successfully');
+    } catch(error){
+        res.status(500).json(error.message);
+    }
+     
+}
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MongoDB).then(() =>{console.log("MongoDB connected")}).catch((err) =>{console.log(err)});
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 8070 ; 
 
 app.listen(PORT,() =>{
     console.log(`Server is up and running on port ${PORT}`);
 });
+
+app.use("/backend/auth", authRoutes);
