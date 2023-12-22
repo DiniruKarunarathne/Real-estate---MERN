113b1dc0a9fecb046cc42a5228e0e318a4376525
Diniru <Dinirukaru65@gmail.com>
2023-12-22 12:28:24 +0530

    complete sign in page ui and functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'; //import bcryptjs (encrypt password)
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'; //import jsonwebtoken (create token)
 
 export const signup = async (req, res , next) => {
     const { username , email, password } = req.body; //get data from frontend
@@ -14,3 +16,29 @@ export const signup = async (req, res , next) => {
     }
      
 }
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body; // Get data from frontend
+  
+    try {
+      const validUser = await User.findOne({ email }); // Find user by email
+      if (!validUser) return next(errorHandler(404, 'User not found'));
+  
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(400, 'Wrong credentials!'));
+  
+      // Create the token first:
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // secret key is in .env file
+
+      const { password: pass, ...rest } = validUser._doc; // Get user info without password
+  
+      res
+        .cookie('access_token', token, { httpOnly: true }) // Set cookie after creating token
+        .status(200)
+        .json(rest);   // Send user data
+    } catch (error) {
+      next(error);
+    }
+  };
+  
