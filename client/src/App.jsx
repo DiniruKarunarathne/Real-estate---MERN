e178f88f80cc2ea1bccbff6753ac366bf9453f45
Diniru <Dinirukaru65@gmail.com>
2023-12-23 23:06:39 +0530

    Update header and profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 
@@ -15,9 +16,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
         <Route path="/sign-In" element={<Signin/>}/>
         <Route path="/signUp" element={<Signup/>}/>
+        <Route element={<PrivateRoute/>}> 
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
       </Routes> 
     </BrowserRouter> // BrowserRouter: Manages navigation between different views in your React app, keeping the URL in sync with what's displayed on the screen. It's like a built-in GPS for our app's user interface.
   )
