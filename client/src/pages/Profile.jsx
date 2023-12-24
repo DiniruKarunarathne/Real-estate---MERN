3718e13f9ee159b8a4966748a3bd3111fdd27f9d
Diniru <Dinirukaru65@gmail.com>
2023-12-24 16:57:22 +0530

    Complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,92 @@
 import { useSelector } from "react-redux"
+import { useRef, useState , useEffect} from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
-  const {currentUser} = useSelector(state => state.user); 
+
+  const fileRef = useRef(null); // Added useRef hook to get access to file input element
+
+  const {currentUser} = useSelector(state => state.user);   // Extracting current user object from Redux store using useSelector
+  
+
+  const [file, setFile] = useState(undefined); // Added file state to store the file object
+
+  const [filePerc , setFilePerc] = useState(0); // Added filePerc state to store the file upload percentage
+
+  const [fileUploadError , setFileUploadError] = useState(false); // Added fileUploadError state to store the file upload error status
+
+  const [formData, setFormData] = useState({}); // Added formData state to store the form data
+
+  
+  
+  //firebase storage rules
+  { /* allow read;
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 && 
+      request.resource.contentType.matches('image/.*') */}
+
+  useEffect(() => {
+    if(file){
+      handleFileUpload(file);
+    }
+  }, [file]) // Added useEffect hook to upload file to firebase storage when file state changes
+
+  
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+   
+    uploadTask.on('state_changed',
+      (snapshot) => { // Progress function 
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100; // Progress percentage
+        setFilePerc(Math.round(progress)); // Setting the percentage state
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => { // On complete function
+        getDownloadURL(uploadTask.snapshot.ref)  // Getting the download url from firebase storage
+          .then((downloadURL) => {    
+            setFormData({ ...formData, avatar: downloadURL }); // Setting the avatar url in the formData state
+            setFileUploadError(false); // Reset the error state on successful upload
+          })
+          .catch((error) => {
+            console.error('Error getting download URL:', error);
+            setFileUploadError(true); 
+          });
+      }
+    );
+  };
+  
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt='profile'  className="rounded-full h-25 w-25 object-cover cursor-pointer self-center mt-2 border-2 border-slate-500"/> 
+
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*"/> {/* Added file input element */}
+
+        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='profile'  className='rounded-full h-25 w-25 object-cover cursor-pointer self-center mt-2 border-2 border-slate-500' style={{ height: "150px", width: "150px", objectFit: "cover" }}/> 
+        
+        <p className="text-sm self-center"> {/* Added paragraph to show file upload status */} 
+
+          {/* Added conditional rendering to show file upload status */}
+          {fileUploadError ? (                                                                                // Check if there's an error during file upload
+            <span className="text-red-700">Error uploading image (image must be less than 2mb)</span>       
+          ) : filePerc > 0 && filePerc < 100 ? (                                                              // Check if file upload is in progress (percentage between 0 and 100)
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (                                                                            // Check if file upload is completed (percentage is 100)
+            <span className="text-green-700">Image successfully uploaded</span>
+          ) : (
+            '' // If none of the above conditions are met, render an empty string
+          )}
+        </p>
+
         <input type="text" placeholder="username" id="username" className="border p-3 rounded-lg" />
         <input type="email" placeholder="email" id="email" className="border p-3 rounded-lg" />
         <input type="text" placeholder="password" id="password" className="border p-3 rounded-lg" />
