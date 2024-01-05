186102007f77815641c02d02731a3ced331cbecf
Diniru <Dinirukaru65@gmail.com>
2024-01-05 12:47:35 +0530

    Create Search route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -69,7 +69,57 @@ export const getListing = async (req, res, next) => {
       next(error);
     }
     
+}
+
+export const getListings = async(req, res , next) =>{
+    try {
+        const limit = parseInt(req.query.limit) || 9    // if limit is not provided, default is 9             
+        const startIndex = parseInt(req.query.startIndex) || 0   // if startIndex is not provided, default is 0
+        
+        let offer = req.query.offer 
+
+        if(offer === undefined || offer === 'false'){
+            offer = { $in: [false , true]}  // if offer is not provided (undefined or false ), search for both true and false
+        }
+
+        let furnished = req.query.furnished
+
+        if(furnished === undefined || furnished === 'false'){
+            furnished = { $in: [false , true]}  
+        }
+
+        let parking = req.query.parking
+
+        if(parking === undefined || parking === 'false'){
+            parking = { $in: [false , true]}  
+        }
+
+        let type = req.query.type
+
+        if(type === undefined || type === 'all'){
+            type = { $in: ['sale' , 'rent']}  // if type is not provided (undefined or all ), search for both sale and rent
+        }
+
+        const searchTerm = req.query.searchTerm || '' ;
+        const sort = req.query.sort || 'createdAt'    ; // if sort is not provided, default is createdAt
+        const order = req.query.order || 'desc' ; 
+
+    
+        const listing = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },    // $regex: searchTerm means that the name of the listing should contain the searchTerm \\ $options: 'i' means that the search is case insensitive(uppercase or lowercase)
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort({ [sort]: order }).limit(limit).skip(startIndex); 
+
+        res.status(200).json(listing);
+
+
+    } catch (error) {
+        next(error);
     }
+}
     
 
 
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing , deleteListing, updateListing, getListing} from "../controllers/listing.controller.js";
+import { createListing , deleteListing, updateListing, getListing , getListings} from "../controllers/listing.controller.js";
 import { verifytoken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/create" , verifytoken, createListing);
 router.delete('/delete/:id', verifytoken, deleteListing);
 router.post('/update/:id', verifytoken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get' , getListings)
 
 export default router;
